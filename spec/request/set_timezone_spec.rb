172a67ba0e6781ee1fc839f623988179a9f8ba12
Karl Baum <karl.baum@gmail.com>
2013-03-12 09:56:49 -0400

    added specs

--- spec/request/set_timezone_spec.rb
+++ spec/request/set_timezone_spec.rb
@@ -3,9 +3,40 @@ require 'spec_helper'
 
 describe 'sets the timezone' do
 
-  it 'sets the timezone properly' do
-    visit '/timezone'
-    save_and_open_page
+  before do
+    #Rails server timezone is UTC...
+    Time.zone =  'UTC'
+  end
+
+  context 'Australia' do
+    before do
+
+      #pretend we're in australia, always wanted to go there
+      page.driver.browser.set_cookie "browser.timezone=Australia/Sydney"
+
+      #visit a page that displays the rails time zone for this request
+      visit '/timezone'
+    end
+
+    it 'sets the timezone properly' do
+      page.should have_content "Australia/Sydney"
+    end
+
+    it 'resets the time zone back to utc after each request' do
+      Time.zone.name.should == 'UTC'
+    end
+  end
+
+  context 'on the first request ever, there will be no cookie, and nothing happens' do
+
+    before do
+      #visit a page that displays the rails time zone for this request
+      visit '/timezone'
+    end
+
+    it 'sets the timezone properly' do
+      page.should have_content "(GMT+00:00) UTC"
+    end
 
   end
 
