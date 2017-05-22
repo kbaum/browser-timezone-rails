c461eac74824730a759b79bcb51f28521591dca6
Cesar Palafox <cpaguila1@gmail.com>
2017-05-22 16:53:47 -0500

    adds support for rails 5, fixes specs

--- spec/dummy/config/initializers/secret_token.rb
+++ spec/dummy/config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Dummy::Application.config.secret_token = 'cc6e88d5b41784239495a56a355e67fc2dfedccb7b5430a2ee9bd7438abd8fb64eab7e7ac6bb0b25d89a9da529864ad4df5c6a40e94b78a3e10e4dcd81768688'
+Dummy::Application.config.secret_key_base = 'cc6e88d5b41784239495a56a355e67fc2dfedccb7b5430a2ee9bd7438abd8fb64eab7e7ac6bb0b25d89a9da529864ad4df5c6a40e94b78a3e10e4dcd81768688'
